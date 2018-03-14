97d92670e790bf8c15478c6e60b97287e0bbe31a
Colten Rouska <crouska@gmail.com>
2018-03-13 18:40:46 -0600

    Adding test files

---
+++ test/unit/utils/uptime-test.js
@@ -0,0 +1,31 @@
+// const { expect } = require('chai');
+const uptime = require('../../../src/utils/uptime');
+
+describe('unit: uptime responses', () => {
+  const times = {
+    twoMins: 100000,
+    oneHour: 10000000,
+    oneDay: 100000000,
+    plusThrity: 10000000000
+  };
+
+  before(() => {
+
+  });
+
+  it('death', () => {
+    console.log(uptime(times.plusThrity));
+  });
+
+  it('does weary', () => {
+    console.log(uptime(times.oneDay));
+  });
+
+  it('does experienced', () => {
+    console.log(uptime(times.oneHour));
+  });
+
+  it('does fresh', () => {
+    console.log(uptime(times.twoMins));
+  });
+});
