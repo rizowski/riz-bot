f91addd161991e83437d476b8f0c33b3cd27373a
Colten Rouska <crouska@gmail.com>
2019-04-26 20:45:16 -0600

    updated docker file, including missed files

---
+++ src/commands/add/level.js
@@ -0,0 +1,14 @@
+const cmd = {
+  title: 'Add a level',
+  description: 'Creates a level that mee6 can assign',
+  requirements: {
+    guild: true,
+  },
+  regex: /^(add|create) level/i,
+  trigger(content) {
+    return cmd.regex.test(content);
+  },
+  action() {},
+};
+
+module.exports = cmd;
